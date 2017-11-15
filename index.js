093f8af550b61a4da4606fb9947abcd387bb2038
Fabio Berger <me@fabioberger.com>
2017-11-15 16:11:08 -0500

    Stop error emission

--- index.js
+++ index.js
@@ -174,7 +174,9 @@ PublishRelease.prototype.publish = function publish () {
       // make sure we do not leak the Github auth token
       err.message = err.message.replace(new RegExp(opts.token, 'g'), '****')
       // we are an EventEmitter so emit the 'error' event so the caller knows we failed.
-      self.emit('error', err)
+      // self.emit('error', err)
+      console.log('Encountered err: ', err);
+      console.log('Deciding to continue execution.');
       // just run the callback with no info. dont run cb(err) beacuse as an EventEmitter this creates a
       // throw Error('Uncaught, unspecified "error" event.')
       // and that error message isn't helpful to anyone
